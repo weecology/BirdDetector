5fd9526735fef6eacb214e6d3b40557be827a37d
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-05-27 06:34:25 -0700

    pad if needed

--- augmentation.py
+++ augmentation.py
@@ -26,7 +26,8 @@ def get_transform(augment):
     """Albumentations transformation of bounding boxs"""
     if augment:
         transform = A.Compose([
-            A.RandomSizedBBoxSafeCrop(height=600, width=600, erosion_rate=0.5,p=0.25),
+            A.PadIfNeeded(600,600),
+            A.RandomSizedBBoxSafeCrop(height=600, width=600, erosion_rate=0.75,p=0.5),
             A.Flip(p=0.5),
             A.pytorch.ToTensorV2()
         ], bbox_params=A.BboxParams(format='pascal_voc',label_fields=["category_ids"]))
