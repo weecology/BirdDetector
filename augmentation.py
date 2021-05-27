24fe3215aaf6c46430f1b8f3effd2d0e250c1c16
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-05-27 11:12:38 -0700

    add pad cv2 border mode

--- augmentation.py
+++ augmentation.py
@@ -27,7 +27,7 @@ def get_transform(augment):
     if augment:
         transform = A.Compose([
             A.PadIfNeeded(600,600, border_mode=cv2.BORDER_CONSTANT, value=0),
-            A.RandomSizedBBoxSafeCrop(height=600, width=600, erosion_rate=0.5,p=0.5),
+            A.RandomSizedBBoxSafeCrop(height=600, width=600, erosion_rate=0.25,p=0.75),
             A.Flip(p=0.5),
             A.pytorch.ToTensorV2()
         ], bbox_params=A.BboxParams(format='pascal_voc',label_fields=["category_ids"]))
