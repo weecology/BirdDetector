7a82080bd848de9aa2d3fb0b8eac4345b0cdcd33
bw4sz <benweinstein2010@gmail.com>
2021-06-08 18:25:02 -0400

    monash

--- generalization.py
+++ generalization.py
@@ -428,7 +428,7 @@ def prepare_monash(generate=True):
                 print("Cannot find corresponding image to annotations {}".format(x))
                 continue
                 
-            annotations = shapefile_to_annotations(shapefile=x, rgb=rgb_path, buffer_size=15)
+            annotations = shapefile_to_annotations(shapefile=x, rgb=rgb_path, buffer_size=0.15)
             annotations["image_path"] = os.path.basename(rgb_path)
             
             if "Claire" in  x:
