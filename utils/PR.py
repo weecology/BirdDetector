fe112ac826e9ab185407385458b010455e648301
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-09 12:08:50 -0700

    upload PR data

--- utils/PR.py
+++ utils/PR.py
@@ -9,6 +9,6 @@ def precision_recall_curve(model, csv_file, root_dir, iou_threshold=0.25):
         test_results = model.evaluate(csv_file=csv_file, root_dir=root_dir, iou_threshold=0.25)
         results.append(pd.DataFrame({"precision":[test_results["box_precision"]],"recall":[test_results["box_recall"]]}))
     results = pd.concat(results)
-    axes = results.plot("recall","precision",style='.-')
+    axes = results.plot(x="recall",y="precision",style='.-')
     return results, axes
     
