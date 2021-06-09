6fafd036a2901ebb1eeda05d1c20587aacd6209d
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-09 11:30:56 -0700

    precision recall curve

---
+++ utils/PR.py
@@ -0,0 +1,14 @@
+#Create PR curve
+import numpy as np
+import pandas as pd
+
+def precision_recall_curve(model, csv_file, root_dir, iou_threshold=0.25):
+    results = []
+    for x in np.arange(0.05,0.75,0.05):
+        model.score_thresh = x
+        test_results = model.evaluate(csv_file=csv_file, root_dir=root_dir, iou_threshold=0.25)
+        results.append(pd.DataFrame({"precision":[test_results["box_precision"]],"recall":[test_results["box_recall"]]}))
+    results = pd.concat(results)
+    axes = results.plot("recall","precision",style='.-')
+    return axes
+    
