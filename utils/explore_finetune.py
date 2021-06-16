8877a77da0edd7ee9717113497979df2294c5a78
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-16 09:19:23 -0700

    look at fine tune hyperparms

--- utils/explore_finetune.py
+++ utils/explore_finetune.py
@@ -2,7 +2,6 @@
 import comet_ml
 from pytorch_lightning.loggers import CometLogger
 from deepforest import main
-import prepare
 
 comet_logger = CometLogger(api_key="ypQZhYfs3nSyKzOfz13iuJpj2",
                             project_name="everglades", workspace="bw4sz",auto_output_logging = "simple")
@@ -10,13 +9,12 @@ comet_logger = CometLogger(api_key="ypQZhYfs3nSyKzOfz13iuJpj2",
 comet_logger.add_tag("fine tune")
 model = main.deepforest.load_from_checkpoint("/orange/ewhite/b.weinstein/generalization/20210616_101934/mckellar_USGS_hayes_terns_penguins_pfeifer_palmyra_everglades.pl")
 model.create_trainer(logger=comet_logger)
-path_dict = prepare.prepare()
-model.config["train"]["csv_file"] = path_dict["monash"]["train"]
-model.config["validation"]["csv_file"] = path_dict["monash"]["test"]
+model.config["train"]["csv_file"] = "/orange/ewhite/b.weinstein/generalization/crops/monash_train.csv"
+model.config["validation"]["csv_file"] = "/orange/ewhite/b.weinstein/generalization/crops/monash_test.csv"
 model.config["validation"]["root_dir"] = "/orange/ewhite/b.weinstein/generalization/crops/"
 model.config["train"]["root_dir"] = "/orange/ewhite/b.weinstein/generalization/crops/"
 
-test_results = model.evaluate(csv_file=path_dict["monash"]["test"], root_dir="/orange/ewhite/b.weinstein/generalization/crops/", iou_threshold=0.25)
+test_results = model.evaluate(csv_file="/orange/ewhite/b.weinstein/generalization/crops/monash_test.csv", root_dir="/orange/ewhite/b.weinstein/generalization/crops/", iou_threshold=0.25)
 
 print("Zero shot Recall is {}".format(test_results["box_recall"]))
 print("Zero shot Precision is {}".format(test_results["box_precision"]))
