d0da63c24de11054167536816fc00deaf273fa35
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-16 09:07:12 -0700

    look at fine tune hyperparms

--- utils/explore_finetune.py
+++ utils/explore_finetune.py
@@ -1,7 +1,14 @@
 #Explore finetuning hyperparameters
+import comet_ml
+from pytorch_lightning.loggers import CometLogger
 from deepforest import main
 from utils import prepare
+
+comet_logger = CometLogger(api_key="ypQZhYfs3nSyKzOfz13iuJpj2",
+                            project_name="everglades", workspace="bw4sz",auto_output_logging = "simple")
+
 model = main.deepforest.load_from_checkpoint("/orange/ewhite/b.weinstein/generalization/20210616_101934/mckellar_USGS_hayes_terns_penguins_pfeifer_palmyra_everglades.pl")
+model.create_trainer(logger=comet_logger)
 path_dict = prepare.prepare()
 model.config["train"]["csv_file"] = path_dict["monash"]["train"]
 model.config["validation"]["csv_file"] = path_dict["monash"]["test"]
