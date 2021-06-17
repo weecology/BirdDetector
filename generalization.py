cca99d7126db0eab230f7b85af0139269c37c28a
bw4sz <benweinstein2010@gmail.com>
2021-06-17 09:58:45 -0400

    merge

--- generalization.py
+++ generalization.py
@@ -198,7 +198,7 @@ if __name__ =="__main__":
     
     #Log commit
     comet_logger.experiment.log_parameter("commit hash",subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip())
-    view_training(path_dict, comet_logger=comet_logger)
+    #view_training(path_dict, comet_logger=comet_logger)
 
     ###leave one out
     train_list = ["mckellar","monash","USGS","hayes","terns","penguins","pfeifer","palmyra","seabirdwatch"]
