cb51baa3146b576e31be1c5ab15b1a301808e07c
bw4sz <benweinstein2010@gmail.com>
2021-06-18 18:44:16 -0400

    add neill

--- generalization.py
+++ generalization.py
@@ -215,7 +215,7 @@ if __name__ =="__main__":
     view_training(path_dict, comet_logger=comet_logger)
 
     ###leave one out
-    train_list = ["USGS","hayes","terns","penguins","pfeifer","palmyra","mckellar","monash"]
+    train_list = ["neill","USGS","hayes","terns","penguins","pfeifer","palmyra","mckellar","monash"]
     results = []
     for x in train_list:
         train_sets = [y for y in train_list if not y==x]
@@ -235,7 +235,7 @@ if __name__ =="__main__":
     comet_logger.experiment.log_metric(name="Mean LOO Precision", value=results.precision.mean())
     
     #Joint model for fine-tuning
-    train_sets = ["monash","terns","penguins","pfeifer","hayes","everglades","USGS","mckellar","palmyra"]
+    train_sets = ["neill","monash","terns","penguins","pfeifer","hayes","everglades","USGS","mckellar","palmyra"]
     test_sets = ["palmyra"]
     recall, precision = train(path_dict=path_dict, config=config, train_sets=train_sets, test_sets=test_sets, comet_logger=comet_logger, save_dir=savedir)
 
