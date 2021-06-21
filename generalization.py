05ef893df870a35609cd063fb2582f7acfefc60a
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-21 11:36:24 -0700

    full run

--- generalization.py
+++ generalization.py
@@ -243,11 +243,10 @@ if __name__ =="__main__":
     comet_logger.experiment.log_parameter("commit hash",subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip())
     comet_logger.experiment.log_parameter("timestamp",timestamp)
     
-    #view_training(path_dict, comet_logger=comet_logger)
+    view_training(path_dict, comet_logger=comet_logger)
 
     ###leave one out
     train_list = ["seabirdwatch","neill","USGS","hayes","terns","penguins","pfeifer","palmyra","mckellar","monash"]
-    train_list = ["penguins"]
     results = []
     for x in train_list:
         train_sets = [y for y in train_list if not y==x]
@@ -280,8 +279,7 @@ if __name__ =="__main__":
     comet_logger.experiment.log_metric(name="Mean Fine Tune Precision", value=mean_fine_tune_precision)
     
     #Joint model for fine-tuning
-    #train_sets = ["seabirdwatch","neill","monash","terns","penguins","pfeifer","hayes","everglades","USGS","mckellar","palmyra"]
-    train_sets = ["palmyra"]
+    train_sets = ["seabirdwatch","neill","monash","terns","penguins","pfeifer","hayes","everglades","USGS","mckellar","palmyra"]
     test_sets = ["palmyra"]
     result = run(path_dict=path_dict,
                             config=config,
