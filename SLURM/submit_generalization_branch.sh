accfe643daa90b4acdb79390d2535aba5633ffd6
bw4sz <benweinstein2010@gmail.com>
2021-06-21 18:20:03 -0400

    more memory

--- SLURM/submit_generalization_branch.sh
+++ SLURM/submit_generalization_branch.sh
@@ -8,8 +8,8 @@ sbatch <<EOT
 #SBATCH --mail-user=benweinstein2010@gmail.com  # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --nodes=1                 # Number of MPI ran
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=90GB
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=100GB
 #SBATCH --time=12:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/home/b.weinstein/logs/DeepForest_%j.out   # Standard output and error log
 #SBATCH --error=/home/b.weinstein/logs/DeepForest_%j.err
