97d74d2713fe7f4060745eb09fc6a9961e5fa75c
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-23 15:01:51 -0700

    change names

--- generalization.py
+++ generalization.py
@@ -133,7 +133,7 @@ def zero_shot(train_sets, test_sets, comet_logger, savedir, config):
     #update backbone weights with new Retinanet head
     model.model = create_model(num_classes=1, nms_thresh=model.config["nms_thresh"], score_thresh=model.config["score_thresh"], backbone=pretrained_DOTA.model.backbone)
     model.config = config
-    model_path = "{}/{}_zeroshot.pt".format(savedir,"_".join(test_sets[0]))
+    model_path = "{}/{}_zeroshot.pt".format(savedir,test_sets[0])
     
     if os.path.exists(model_path):
         print("loading {}".format(model_path))
@@ -166,7 +166,7 @@ def fine_tune(dataset, comet_logger, savedir, config):
     model_path = "{}/{}_finetune.pt".format(savedir, dataset)
     model = BirdDetector(transforms = deepforest_transform)   
     model.config = config
-    weights = "{}/{}_zeroshot.pt".format(savedir,"_".join(dataset))
+    weights = "{}/{}_zeroshot.pt".format(savedir,dataset)
     model.model.load_state_dict(torch.load(weights))
     
     if os.path.exists(model_path):
@@ -191,10 +191,10 @@ def mini_fine_tune(dataset, comet_logger, config, savedir):
     #Fine tuning, up to 1000 birds from train
     min_annotation_results = []
     for i in range(5):
-        model_path = "{}/{}_mini_{}".format(savedir, dataset,i)
+        model_path = "{}/{}_mini_{}.pt".format(savedir, dataset,i)
         model = BirdDetector(transforms = deepforest_transform)   
         model.config = config
-        weights = "{}/{}_zeroshot.pt".format(savedir,"_".join(dataset))
+        weights = "{}/{}_zeroshot.pt".format(savedir,dataset)
         model.model.load_state_dict(torch.load(weights))
         
         if os.path.exists(model_path):
