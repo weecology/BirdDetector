31fcf84ed0406100873c4086e63658ab1bd89cce
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-25 15:37:05 -0700

    create test figures

--- predict_test.py
+++ predict_test.py
@@ -26,4 +26,7 @@ if __name__ == "__main__":
     for x in path_dict:
         if x == "everglades":
             continue
-        run(x)
+        try:
+            run(x)
+        except:
+            pass
