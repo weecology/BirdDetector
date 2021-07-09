a50a2e8840e3a5f43245b04bb04d2a06c8ba068b
bw4sz <benweinstein2010@gmail.com>
2021-07-09 13:42:41 -0400

    8 workers

--- SLURM/submit_single.sh
+++ SLURM/submit_single.sh
@@ -8,7 +8,7 @@ sbatch <<EOT
 #SBATCH --mail-user=benweinstein2010@gmail.com  # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --nodes=1                 # Number of MPI ran
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=50GB
 #SBATCH --time=48:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/home/b.weinstein/logs/DeepForest_%j.out   # Standard output and error log
