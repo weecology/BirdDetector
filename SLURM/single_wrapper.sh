14972a1cc75ac666a48d04495dab4f97fa00fe6e
bw4sz <benweinstein2010@gmail.com>
2021-07-12 22:13:00 -0400

    two results

--- SLURM/single_wrapper.sh
+++ SLURM/single_wrapper.sh
@@ -1,6 +1,6 @@
 #Wrapper for submitting seperate SLURM runs
 #run from top dir
-for DATASET in newmexico neill seabirdwatch penguins pfeifer palmyra monash mckellar USGS terns hayes
+for DATASET in newmexico neill seabirdwatch penguins pfeifer palmyra mckellar USGS terns hayes monash
 do
     sbatch SLURM/submit_single.sh $1 $2 $DATASET
 done
