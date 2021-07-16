c3daf9ec238c9be9f2ebbb754155d6f1d3bef95a
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-07-16 11:31:28 -0700

    new images

--- Figures/create_figure_annotations.py
+++ Figures/create_figure_annotations.py
@@ -9,19 +9,19 @@ import shutil
 
 # Figure 3
 #A
-shutil.copy2(src="/blue/ewhite/b.weinstein/generalization/crops/seabirds_rgb_564.png", dst="seabirds_rgb_564.png")
+shutil.copy2(src="/blue/ewhite/b.weinstein/generalization/crops/seabirds_rgb_775.png", dst="seabirds_rgb_775.png")
 ground_truth = pd.read_csv("/blue/ewhite/b.weinstein/generalization/crops/terns_test.csv")
-ground_truth = ground_truth[ground_truth.image_path == "seabirds_rgb_564.png"]
+ground_truth = ground_truth[ground_truth.image_path == "seabirds_rgb_775.png"]
 ground_truth["geometry"] = ground_truth.apply(lambda x: geometry.box(x["xmin"],-x["ymin"],x["xmax"],-x["ymax"]), axis = 1)
 ground_truth = gpd.GeoDataFrame(ground_truth)
-ground_truth.to_file("seabirds_rgb_564_annotations.shp")
+ground_truth.to_file("seabirds_rgb_775_annotations.shp")
 
 m = main.deepforest()
 m.model.load_state_dict(torch.load("/blue/ewhite/b.weinstein/generalization/snapshots/terns_zeroshot.pt"))
-boxes = m.predict_image(path = "/blue/ewhite/b.weinstein/generalization/crops/seabirds_rgb_564.png")
+boxes = m.predict_image(path = "/blue/ewhite/b.weinstein/generalization/crops/seabirds_rgb_775.png")
 boxes["geometry"] = boxes.apply(lambda x: geometry.box(x["xmin"],-x["ymin"],x["xmax"],-x["ymax"]), axis = 1)
 boxes = gpd.GeoDataFrame(boxes)
-boxes.to_file("seabirds_rgb_564_predictions.shp")
+boxes.to_file("seabirds_rgb_775_predictions.shp")
 
 #B
 shutil.copy2(src="/blue/ewhite/b.weinstein/generalization/crops/Rzepecki Islands_south_2016_Chinstrap_penguins_76.png", dst="Rzepecki Islands_south_2016_Chinstrap_penguins_76.png")
@@ -67,3 +67,50 @@ boxes = m.predict_image(path = "/blue/ewhite/b.weinstein/generalization/crops/St
 boxes["geometry"] = boxes.apply(lambda x: geometry.box(x["xmin"],-x["ymin"],x["xmax"],-x["ymax"]), axis = 1)
 boxes = gpd.GeoDataFrame(boxes)
 boxes.to_file("SteepleJason_Hump_Nov2019_transparent_mosaic_group1_predictions.shp")
+
+
+#Figure 4
+#A
+shutil.copy2(src="/blue/ewhite/b.weinstein/generalization/crops/BDA_18A4_20181107_1_38.png", dst="BDA_18A4_20181107_1_38.png")
+ground_truth = pd.read_csv("/blue/ewhite/b.weinstein/generalization/crops/newmexico_test.csv")
+ground_truth = ground_truth[ground_truth.image_path == "BDA_18A4_20181107_1_38.png"]
+ground_truth["geometry"] = ground_truth.apply(lambda x: geometry.box(x["xmin"],-x["ymin"],x["xmax"],-x["ymax"]), axis = 1)
+ground_truth = gpd.GeoDataFrame(ground_truth)
+ground_truth.to_file("BDA_18A4_20181107_1_38.shp")
+
+m = main.deepforest()
+m.model.load_state_dict(torch.load("/blue/ewhite/b.weinstein/generalization/snapshots/newmexico_zeroshot.pt"))
+boxes = m.predict_image(path = "/blue/ewhite/b.weinstein/generalization/crops/BDA_18A4_20181107_1_38.png")
+boxes["geometry"] = boxes.apply(lambda x: geometry.box(x["xmin"],-x["ymin"],x["xmax"],-x["ymax"]), axis = 1)
+boxes = gpd.GeoDataFrame(boxes)
+boxes.to_file("BDA_18A4_20181107_1_38_predictions.shp")
+
+#B
+shutil.copy2(src="/blue/ewhite/b.weinstein/generalization/crops/KIPPa2016a_000144_3.png", dst="KIPPa2016a_000144_3.png")
+ground_truth = pd.read_csv("/blue/ewhite/b.weinstein/generalization/crops/seabirdwatch_test.csv")
+ground_truth = ground_truth[ground_truth.image_path == "KIPPa2016a_000144_3.png"]
+ground_truth["geometry"] = ground_truth.apply(lambda x: geometry.box(x["xmin"],-x["ymin"],x["xmax"],-x["ymax"]), axis = 1)
+ground_truth = gpd.GeoDataFrame(ground_truth)
+ground_truth.to_file("KIPPa2016a_000144_3.shp")
+
+m = main.deepforest()
+m.model.load_state_dict(torch.load("/blue/ewhite/b.weinstein/generalization/snapshots/seabirdwatch_zeroshot.pt"))
+boxes = m.predict_image(path = "/blue/ewhite/b.weinstein/generalization/crops/KIPPa2016a_000144_3.png")
+boxes["geometry"] = boxes.apply(lambda x: geometry.box(x["xmin"],-x["ymin"],x["xmax"],-x["ymax"]), axis = 1)
+boxes = gpd.GeoDataFrame(boxes)
+boxes.to_file("KIPPa2016a_000144_3_predictions.shp")
+
+#C
+shutil.copy2(src="/blue/ewhite/b.weinstein/generalization/crops/JackfishLakeBLTE_Sony_1_373.png", dst="JackfishLakeBLTE_Sony_1_373.png")
+ground_truth = pd.read_csv("/blue/ewhite/b.weinstein/generalization/crops/mckellar_test.csv")
+ground_truth = ground_truth[ground_truth.image_path == "JackfishLakeBLTE_Sony_1_373.png"]
+ground_truth["geometry"] = ground_truth.apply(lambda x: geometry.box(x["xmin"],-x["ymin"],x["xmax"],-x["ymax"]), axis = 1)
+ground_truth = gpd.GeoDataFrame(ground_truth)
+ground_truth.to_file("JackfishLakeBLTE_Sony_1_373.shp")
+
+m = main.deepforest()
+m.model.load_state_dict(torch.load("/blue/ewhite/b.weinstein/generalization/snapshots/mckellar_zeroshot.pt"))
+boxes = m.predict_image(path = "/blue/ewhite/b.weinstein/generalization/crops/JackfishLakeBLTE_Sony_1_373.png")
+boxes["geometry"] = boxes.apply(lambda x: geometry.box(x["xmin"],-x["ymin"],x["xmax"],-x["ymax"]), axis = 1)
+boxes = gpd.GeoDataFrame(boxes)
+boxes.to_file("JackfishLakeBLTE_Sony_1_373_predictions.shp")
