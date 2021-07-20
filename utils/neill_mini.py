a24b2fca1cddada32a9defd7949b85d3daaa8c91
bw4sz <benweinstein2010@gmail.com>
2021-07-20 14:31:15 -0400

    merge

--- utils/neill_mini.py
+++ utils/neill_mini.py
@@ -1,15 +1,14 @@
-#Everglades 1000 bird test, entirely new code to check results from other parts of the analysis
+#neill 1000 bird test, entirely new code to check results from other parts of the analysis
 import comet_ml
 from deepforest import main
 from pytorch_lightning import loggers
 import pandas as pd
 import random
 import tempfile
-import glob
 
-comet_logger = loggers.CometLogger(project_name="everglades", workspace="bw4sz",auto_output_logging = "simple")
-train = pd.read_csv("/blue/ewhite/b.weinstein/generalization/crops/everglades_train.csv")
-comet_logger.experiment.add_tag("Everglades")
+comet_logger = loggers.CometLogger(project_name="neill", workspace="bw4sz",auto_output_logging = "simple")
+train = pd.read_csv("/blue/ewhite/b.weinstein/generalization/crops/neill_train.csv")
+comet_logger.experiment.add_tag("neill")
 
 train_images = train.image_path.unique()
 random.shuffle(train_images)
@@ -26,17 +25,17 @@ while counter < 1000:
 sampled_annotations = pd.concat(sampled_annotations)
 sampled_annotations.to_csv("{}/annotations.csv".format(tmpdir))
 m = main.deepforest(label_dict={"Bird":0})
-m.use_release()
+
 m.config["train"]["csv_file"] = "{}/annotations.csv".format(tmpdir)
 m.config["train"]["root_dir"] = "/blue/ewhite/b.weinstein/generalization/crops/"
     
-m.config["validation"]["csv_file"] = "/blue/ewhite/b.weinstein/generalization/crops/everglades_test.csv"
+m.config["validation"]["csv_file"] = "/blue/ewhite/b.weinstein/generalization/crops/neill_test.csv"
 m.config["validation"]["root_dir"] = "/blue/ewhite/b.weinstein/generalization/crops/"
-m.config["train"]["epochs"] = 30
+m.config["epochs"] = 10
 m.create_trainer(logger=comet_logger)
 m.trainer.fit(m)
 
-results = m.evaluate(csv_file=m.config["validation"]["csv_file"], root_dir=m.config["validation"]["root_dir"], iou_threshold=0.2, savedir=tmpdir)
+results = m.evaluate(csv_file=m.config["validation"]["csv_file"], root_dir=m.config["validation"]["root_dir"], iou_threshold=0.2)
 recall = results["box_recall"]
 precision = results["box_precision"]
 
@@ -44,12 +43,3 @@ comet_logger.experiment.log_metric("Recall",recall)
 comet_logger.experiment.log_metric("Precision",precision)
 
 
-results = m.evaluate(csv_file=m.config["train"]["csv_file"], root_dir=m.config["train"]["root_dir"], iou_threshold=0.2)
-recall = results["box_recall"]
-precision = results["box_precision"]
-
-comet_logger.experiment.log_metric("Train Recall",recall)
-comet_logger.experiment.log_metric("Train Precision",precision)
-
-for x in glob.glob("{}/*.png".format(tmpdir)):
-    comet_logger.experiment.log_image(x, image_scale=0.25)
