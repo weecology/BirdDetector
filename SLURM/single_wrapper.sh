8e98bc4059ed939b0338c3b441ba3d2194584eb8
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-07-21 16:26:13 -0700

    add michigan data

--- SLURM/single_wrapper.sh
+++ SLURM/single_wrapper.sh
@@ -1,6 +1,6 @@
 #Wrapper for submitting seperate SLURM runs
 #run from top dir
-for DATASET in newmexico neill seabirdwatch penguins pfeifer palmyra mckellar USGS terns hayes monash
+for DATASET in newmexico neill seabirdwatch penguins pfeifer palmyra mckellar USGS terns hayes monash michigan
 do
     sbatch SLURM/submit_single.sh $1 $2 $DATASET
 done
