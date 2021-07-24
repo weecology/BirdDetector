998a61822dfec0c7c46072a40f25c5f367477fc4
bw4sz <benweinstein2010@gmail.com>
2021-07-23 21:32:21 -0400

    merge

--- utils/prepare.py
+++ utils/prepare.py
@@ -993,7 +993,7 @@ def prepare():
     paths["USGS"] = prepare_USGS(generate=False)
     paths["monash"] = prepare_monash(generate=False)
     paths["mckellar"] = prepare_mckellar(generate=False)
-    paths["seabirdwatch"] = prepare_seabirdwatch(generate=True)
+    paths["seabirdwatch"] = prepare_seabirdwatch(generate=False)
     paths["neill"] = prepare_neill(generate=True)
     paths["newmexico"] = prepare_newmexico(generate=True)
     paths["valle"] = prepare_valle(generate=True)
