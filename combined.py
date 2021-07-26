e8a40942e9ac61a5cfdca465308c909658776641
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-07-26 14:31:59 -0700

    update combined

--- combined.py
+++ combined.py
@@ -34,13 +34,13 @@ for x in train_sets:
 train_annotations = pd.concat(all_sets)
 
 #A couple illegal boxes, make slightly smaller
-train_annotations["xmin"] = train_annotations["xmin"].astype(float) 
-train_annotations["xmax"] = train_annotations["xmax"].astype(float) - 3
-train_annotations["ymin"] = train_annotations["ymin"].astype(float)
-train_annotations["ymax"] = train_annotations["ymax"].astype(float) - 3
+#train_annotations["xmin"] = train_annotations["xmin"].astype(float) 
+#train_annotations["xmax"] = train_annotations["xmax"].astype(float) - 3
+#train_annotations["ymin"] = train_annotations["ymin"].astype(float)
+#train_annotations["ymax"] = train_annotations["ymax"].astype(float) - 3
 
-train_annotations = train_annotations[~(train_annotations.xmin >= train_annotations.xmax)]
-train_annotations = train_annotations[~(train_annotations.ymin >= train_annotations.ymax)]
+#train_annotations = train_annotations[~(train_annotations.xmin >= train_annotations.xmax)]
+#train_annotations = train_annotations[~(train_annotations.ymin >= train_annotations.ymax)]
 train_annotations.to_csv("/blue/ewhite/b.weinstein/generalization/crops/training_annotations.csv")
 
 all_val_sets = []
@@ -53,13 +53,6 @@ test_annotations.to_csv("/blue/ewhite/b.weinstein/generalization/crops/test_anno
 comet_logger.experiment.log_parameter("training_images",len(train_annotations.image_path.unique()))
 comet_logger.experiment.log_parameter("training_annotations",train_annotations.shape[0])
 
-train_df = pd.read_csv("/blue/ewhite/b.weinstein/AerialDetection/data/trainval1024/train.csv")
-label_dict = {x: index for index, x in enumerate(train_df.label.unique())}    
-pretrained_DOTA = main.deepforest(num_classes=15, label_dict=label_dict)
-model = BirdDetector(transforms = get_transform)
-
-#update backbone weights with new Retinanet head
-model.model = create_model(num_classes=1, nms_thresh=model.config["nms_thresh"], score_thresh=model.config["score_thresh"], backbone=pretrained_DOTA.model.backbone)
-model.config = config
-model = fit(model, train_annotations, comet_logger, "palmyra_combined")
+all_annotations = pd.concat([train_annotations, test_annotations])
+model = fit(model, all_annotations, comet_logger, "palmyra_combined")
 torch.save(model.model.state_dict(),"/blue/ewhite/b.weinstein/generalization/snapshots/combined.pt")
