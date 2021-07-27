c5e941a82548832b5e0b19ee902725d07d875a3d
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-07-27 11:43:45 -0700

    higher learning rate

--- random_weight.py
+++ random_weight.py
@@ -20,11 +20,11 @@ model = BirdDetector(transforms = deepforest_transform)
 model.config["validation"]["csv_file"] = "/blue/ewhite/b.weinstein/generalization/crops/{}_test.csv".format(dataset)
 model.config["validation"]["root_dir"] = "/blue/ewhite/b.weinstein/generalization/crops/"
 model.config["epochs"] = 20
-model.config["lr"] = 0.02
+model.config["lr"] = 0.2
 model = fit(model, train_annotations, comet_logger,"{}_random_{}".format(dataset, n))
 finetune_results = model.evaluate(csv_file="/blue/ewhite/b.weinstein/generalization/crops/{}_test.csv".format(dataset), root_dir="/blue/ewhite/b.weinstein/generalization/crops/", iou_threshold=0.2)
 comet_logger.experiment.log_metric("Box Recall {} {}".format(n,dataset),finetune_results["box_recall"])
-comet_logger.experiment.log_metric("Box Recall {} {}".format(n,dataset),finetune_results["box_precision"])
+comet_logger.experiment.log_metric("Box Precision {} {}".format(n,dataset),finetune_results["box_precision"])
 
 
 
