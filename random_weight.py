1d91d3171d6f593c816eacde23aa5fb165652cc7
bw4sz <benweinstein2010@gmail.com>
2021-07-28 10:04:19 -0400

    1 gpu

--- random_weight.py
+++ random_weight.py
@@ -15,7 +15,8 @@ comet_logger.experiment.add_tag(dataset)
 
 df = pd.read_csv("/blue/ewhite/b.weinstein/generalization/crops/{}_train.csv".format(dataset))  
 n = df.shape[0]
-#train_annotations = select(df, n=n)
+n=5000
+train_annotations = select(df, n=n)
 model = BirdDetector(transforms = deepforest_transform)   
 #model.config["validation"]["csv_file"] = "/blue/ewhite/b.weinstein/generalization/crops/{}_test.csv".format(dataset)
 #model.config["validation"]["root_dir"] = "/blue/ewhite/b.weinstein/generalization/crops/"
