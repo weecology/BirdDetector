338a2aba2e5f87369037d8e2c8669cc4c7598cca
bw4sz <benweinstein2010@gmail.com>
2021-07-29 22:23:31 -0400

    random weight

--- random_weight.py
+++ random_weight.py
@@ -6,7 +6,7 @@ from generalization import *
 from model import BirdDetector
 import tempfile
 
-dataset = "palmyra"
+dataset = "newmexico"
 tmpdir = tempfile.gettempdir()
 
 comet_logger = loggers.CometLogger(project_name="everglades", workspace="bw4sz",auto_output_logging = "simple")
@@ -20,9 +20,9 @@ n=1000
 train_annotations = select(df, n=n)
 #model.config["validation"]["csv_file"] = "/blue/ewhite/b.weinstein/generalization/crops/{}_test.csv".format(dataset)
 #model.config["validation"]["root_dir"] = "/blue/ewhite/b.weinstein/generalization/crops/"
-model.config["train"]["epochs"] = 50
+model.config["train"]["epochs"] = 200
 model.config["train"]["lr"] = 0.001
-model = fit(model, train_annotations, comet_logger,"{}_random_{}".format(dataset, n), validation=True)
+model = fit(model, df, comet_logger,"{}_random_{}".format(dataset, n), validation=True)
 finetune_results = model.evaluate(csv_file="/blue/ewhite/b.weinstein/generalization/crops/{}_test.csv".format(dataset), root_dir="/blue/ewhite/b.weinstein/generalization/crops/", iou_threshold=0.2)
 comet_logger.experiment.log_metric("Box Recall {} {}".format(n,dataset),finetune_results["box_recall"])
 comet_logger.experiment.log_metric("Box Precision {} {}".format(n,dataset),finetune_results["box_precision"])
